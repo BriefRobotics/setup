d99938bd299e825ac0df8a63739f60988ff57c8b
AshleyF <ashley.feniello@live.com>
2014-10-28 14:11:56 -0700

    Fn key setting for MacBook

--- setup-ashleyf.sh
+++ setup-ashleyf.sh
@@ -1,2 +1,8 @@
 git config --global user.name "AshleyF"
 git config --global user.email "ashley.feniello@live.com"
+
+# apple keyboard function keys
+# 0 = Fn key disabled
+# 1 = Fn key pressed by default (Apple's default)
+# 2 = Fn key released by default
+echo 2 > /sys/module/hid_apple/parameters/fnmode
