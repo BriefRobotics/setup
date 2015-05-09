b7fbe21315b9681384f136bef586b787a70728b1
AshleyF <ashley.feniello@live.com>
2015-05-09 17:51:39 -0400

    Breaking apart setup scripts

--- setup.sh
+++ setup.sh
@@ -9,26 +9,6 @@ apt-get -y install tmux
 apt-get -y install unity-tweak-tool
 apt-get -y install gnome-tweak-tool
 
-apt-get -y install git
-git config --global push.default simple
-
-# apt-get -y install atom
-# apm install vim-mode
-
-apt-get -y install mono-complete
-apt-get -y install fsharp
-
-apt-get -y install exuberant-ctags
-cp -f ./.ctags ~/.ctags
-
-# clojure
-apt-get -y install default-jre
-cp lein /usr/local/bin
-chmod +x /usr/local/bin/lein
-
 apt-get -y install gcc
-
-apt-get -y install zsh
-curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh # DANGER!
 
 apt-get -y install htop
---
+++ zsh.sh
@@ -0,0 +1,4 @@
+#!/bin/bash
+
+apt-get -y install zsh
+curl -L https://raw.github.com/robbyrussell/oh-my-zsh/master/tools/install.sh | sh # DANGER!
