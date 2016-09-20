87f870504bdf31fd4ccaced740ffcc38755f5a8a
ashleyf <ashley.feniello@live.com>
2016-09-19 18:11:28 -0700

    Nao robot setup

--- ros/nao.md
+++ ros/nao.md
@@ -1,4 +1,5 @@
 # Nao Robot Setuudo apt-get install ros-.*-nao.robot
 
 * [Documentation](doc.aldebaran.com/2-4/home_nao.html)
-* [SDK Download](https://comunity.ald.softbankrobotics.com/en/resources/software/language/en-gb)
+* [SDK Download](https://community.ald.softbankrobotics.com/en/resources/software/language/en-gb)
+* [Nao Bringup](http://wiki.ros.org/nao_bringup)
--- ros/nao.sh
+++ ros/nao.sh
@@ -1,5 +1,23 @@
 #!/bin/bash
 
+# http://wiki.ros.org/nao
+
 sudo apt-get install ros-.*-nao.robot
 sudo apt-get install ros-.*-nao.extras
 
+sudo apt-get install ros-indigo-nao-meshes
+
+sudo apt-get install ros-indigo-driver-base ros-indigo-move-base-msgs ros-indigo-octomap ros-indigo-octomap-msgs ros-indigo-humanoid-msgs ros-indigo-humanoid-nav-msgs ros-indigo-camera-info-manager ros-indigo-camera-info-manager-py
+
+# http://wiki.ros.org/nao/Tutorials/Installation
+
+echo "Download the following from https://community.ald.softbankrobotics.com/en/resources/software/language/en-gb"
+echo "* naoqi-sdk*linux64.tar.gz"
+echo "* pynaoqi-python*linux64.tar.gz"
+
+mkdir ~/naoqi
+cp ~/Downloads/naoqi-sdk-*-linux64.tar.gz ~/Downloads/pynaoqi-python*linux64.tar.gz ~/naoqi
+cd ~/naoqi
+
+tar xzf naoqi-sdk*linux64.tar.gz
+tar xzf pynaoqi-python*linux64.tar.gz
